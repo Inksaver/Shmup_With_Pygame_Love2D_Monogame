baa929c9affa175cf2261c346ddf2b4ecc01f243
Inksaver <Inksaver@users.noreply.github.com>
2022-01-15 07:04:01 +0000

    Add files via upload

---
+++ Episode01-Move_Player/Monogame/Shmup/Shared.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+namespace Shmup
+{
+    /// <summary>
+    /// Public static class as a repository for global variables
+    /// </summary>
+    internal static class Shared
+    {
+        public static int WIDTH = 480;
+        public static int HEIGHT = 600;
+        public static Dictionary<string, int> gameStates = new Dictionary<string, int>
+        {
+            {"menu", 1},
+            {"play", 2},
+            {"quit", 3}
+        };
+        public static int gameState = 1; // default gamestate (menu)
+    }
+}
