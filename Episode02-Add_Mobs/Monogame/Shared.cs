b8c723b8ed20b99a4ef6c7af799641dfd8875ef3
Inksaver <Inksaver@users.noreply.github.com>
2022-01-17 12:05:36 +0000

    Update Shared.cs

--- Episode02-Add_Mobs/Monogame/Shared.cs
+++ Episode02-Add_Mobs/Monogame/Shared.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Shmup
 {
@@ -7,14 +10,15 @@ namespace Shmup
         /// <summary>
         /// This static class is a shared repository of global variables
         /// </summary>
-        public static int WIDTH = 480;
-        public static int HEIGHT = 600;
+        public static int WIDTH = 480;      // Window width set here
+        public static int HEIGHT = 600;     // Window height set here
         public static Dictionary<string, int> gameStates = new Dictionary<string, int>
         {
             {"menu", 1},
             {"play", 2},
             {"quit", 3}
         };
-        public static int gameState = 1; // default gamestate
+        public static int gameState = 1;    // default gamestate (menu)
+        public static Color GREEN = new Color(0,255,0); // Color.Green is (0, 128, 0) half as bright
     }
 }
