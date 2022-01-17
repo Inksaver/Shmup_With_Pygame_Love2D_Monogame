8277a7709791a2d6a438c860d638b8d87ba13079
Inksaver <Inksaver@users.noreply.github.com>
2022-01-17 12:01:49 +0000

    Update Shared.cs

--- Episode01-Move_Player/Monogame/Shared.cs
+++ Episode01-Move_Player/Monogame/Shared.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 namespace Shmup
 {
     /// <summary>
@@ -6,14 +7,15 @@ namespace Shmup
     /// </summary>
     internal static class Shared
     {
-        public static int WIDTH = 480;
-        public static int HEIGHT = 600;
+        public static int WIDTH = 480;      // Window width set here
+        public static int HEIGHT = 600;     // Window height set here
         public static Dictionary<string, int> gameStates = new Dictionary<string, int>
         {
             {"menu", 1},
             {"play", 2},
             {"quit", 3}
         };
-        public static int gameState = 1; // default gamestate (menu)
+        public static int gameState = 1;    // default gamestate (menu)
+        public static Color GREEN = new Color(0,255,0); // Color.Green is (0, 128, 0) half as bright
     }
 }
