7df5739b7df5c4df63c8403483c4fa471dfc0bb2
Inksaver <Inksaver@users.noreply.github.com>
2022-01-27 14:20:24 +0000

    Update shared.lua

--- Episode01-Move_Player/Love2D/shared.lua
+++ Episode01-Move_Player/Love2D/shared.lua
@@ -1,19 +1,19 @@
 --[[ lua equivalent of C# static class and Python code module ]]
 local S = {}
-S.WIDTH 		= love.graphics.getWidth()
-S.HEIGHT 		= love.graphics.getHeight()
+S.WIDTH 		  = love.graphics.getWidth()
+S.HEIGHT 		  = love.graphics.getHeight()
 -- colour constants: 0-1 each value
-S.BLACK 		= {0, 0, 0}
-S.WHITE 		= {1, 1, 1}
-S.RED 			= {1, 0, 0}
-S.GREEN 		= {0, 1, 0}
-S.BLUE 			= {0 ,0, 1}
-S.CYAN 			= {0, 1, 1}
+S.BLACK 		  = {0, 0, 0}
+S.WHITE 		  = {1, 1, 1}
+S.RED 			  = {1, 0, 0}
+S.GREEN 		  = {0, 1, 0}
+S.BLUE 			  = {0 ,0, 1}
+S.CYAN 			  = {0, 1, 1}
 S.MAGENTA 		= {1, 0, 1}
-S.YELLOW 		= {1 ,1, 0}
+S.YELLOW 		  = {1 ,1, 0}
 S.DARKBLUE		= {0, 0, 12/255}
 -- variables
-S.debug			= false
+S.debug			  = false
 S.gamestates	= {menu = 1, play = 2, leaderboard = 3, quit = 4}
 S.gamestate		= 1
 
