235147b548307716d4ec4b24f79c5b7c790df607
Inksaver <Inksaver@users.noreply.github.com>
2022-01-27 22:21:30 +0000

    Update mob.lua

--- Episode02-Add_Mobs/Love2D/mob.lua
+++ Episode02-Add_Mobs/Love2D/mob.lua
@@ -1,42 +1,40 @@
 local Class = require("lib.Class")
 local Mob = Class:derive("Mob")
+local Rectangle = require "lib.Rectangle"
 local Shared = require "shared"
 
 function Mob:new(width, height)
 	--[[ class constructor ]]
-	self.w = width
-	self.h = height
-	Mob.setProperties(self)
+	self.rect = Rectangle(0, 0, width, height)
+	self:setProperties()
 end
 
 function Mob.setProperties(self)
 	-- set speed to a random amount, so some will move faster than others
-	self.x = math.random(0, Shared.WIDTH - self.w)	-- make the mob appear on screen randomly across it's width
-	self.y = math.random(-150, -100)			-- start off the top of the screen by random amount
-	self.speedY = math.random(40, 600)
-	self.speedX = math.random(-200, 200)
+	self.rect.x = math.random(0, Shared.WIDTH - self.rect.width)	-- make the mob appear on screen randomly across it's width
+	self.rect.y = math.random(-150, -100)					-- start off the top of the screen by random amount
+	self.speedY = math.random(50, 600)
+	self.speedX = math.random(-100, 100)
+	if Shared.debug then 											-- reduce the speed
+		self.speedY = math.random(10, 100)
+		self.speedX = math.random(-20, 20)
+	end
 end
 
 function Mob:getRect()
-	rect = {}
-	rect.x = self.x
-	rect.y = self.y
-	rect.w = self.w
-	rect.h = self.h
-	return rect
+	return self.rect
 end
 
 function Mob:update(dt)
-	self.y = self.y + self.speedY * dt
-	self.x = self.x + self.speedX * dt
-	-- check if Mobob has gone off bottom or sides of screen
-	if self.y >Shared.HEIGHT or self.x < 0 - self.w or self.x > Shared.WIDTH then
+	self.rect:updatePosition(self.speedX * dt, self.speedY * dt)
+	-- check if Mob has gone off bottom or sides of screen
+	if self.rect.top > Shared.HEIGHT or self.rect.right < 0 or self.rect.left > Shared.WIDTH then
 		Mob.setProperties(self) -- reset to position above top of the screen
 	end
 end
 
 function Mob:draw()
-	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+	love.graphics.rectangle("fill", self.rect.x, self.rect.y, self.rect.w, self.rect.h)
 end
 
 return Mob
