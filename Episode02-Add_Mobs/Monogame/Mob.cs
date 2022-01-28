019c4df78366aa3940489abb9d3645d665376143
Inksaver <Inksaver@users.noreply.github.com>
2022-01-28 06:44:54 +0000

    Update Mob.cs

--- Episode02-Add_Mobs/Monogame/Mob.cs
+++ Episode02-Add_Mobs/Monogame/Mob.cs
@@ -10,41 +10,41 @@ namespace Shmup
         /// <summary>
         /// Mob class for generating meteors from top of window
         /// </summary>
-        #region Private Class variables
-        private RectangleF Rectangle;
-        private float SpeedY;
-        private float SpeedX;
-        private Color Colour;
+        #region Class variables
+        public RectangleF Rectangle;
+        private float speedY;
+        private float speedX;
+        private Color colour;
         private Random random;
         #endregion
         #region Constructor
-        public Mob(int width, int height, Color colour)
+        public Mob(int width, int height, Color clr)
         {
             random = new Random();
             Rectangle = new RectangleF(0, 0, width, height);
-            Colour = colour;
+            colour = clr;
             SetProperties();
         }
         #endregion
         #region Class Methods
+        public void SetProperties()
+        {
+            Rectangle.X = random.Next(0, Shared.WIDTH - (int)Rectangle.Width);
+            Rectangle.Y = random.Next(-150, -50);
+            Rectangle.Y = 0;
+            speedY = random.Next(150, 600);
+            speedX = random.Next(-50, 50);
+        }
         public void Update(float dt)
         {
-            Rectangle.X += SpeedX * dt;
-            Rectangle.Y += SpeedY * dt;
+            Rectangle.X += speedX * dt;
+            Rectangle.Y += speedY * dt;
             if (Rectangle.Y > Shared.HEIGHT || Rectangle.X < 0 - Rectangle.Width || Rectangle.X > Shared.WIDTH)
                 SetProperties();
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.FillRectangle( rectangle: Rectangle, color: Colour);
-        }
-        private void SetProperties()
-        {
-            Rectangle.X = random.Next(0, Shared.WIDTH - (int)Rectangle.Width);
-            Rectangle.Y = random.Next(-150, -50);
-            Rectangle.Y = 0;
-            SpeedY = random.Next(50, 300);
-            SpeedX = random.Next(-50, 50);
+            spriteBatch.FillRectangle( rectangle: Rectangle, color: colour);
         }
         #endregion
     }
