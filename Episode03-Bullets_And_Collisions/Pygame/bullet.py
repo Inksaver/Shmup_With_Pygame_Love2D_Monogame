e3a56cfbfd1aef064657c335c02e3b26c664c9ca
Inksaver <Inksaver@users.noreply.github.com>
2022-01-28 12:53:01 +0000

    Update bullet.py

--- Episode03-Bullets_And_Collisions/Pygame/bullet.py
+++ Episode03-Bullets_And_Collisions/Pygame/bullet.py
@@ -2,10 +2,9 @@ import pygame
 import shared
 
 class Bullet():
-	def __init__(self, img, x, y) -> None:
-		self.image = img
-		self.image.set_colorkey(shared.BLACK)		
-		self.rect = self.image.get_rect() # filled rectangle as taken from image
+	def __init__(self, x:int, y:int, w:int, h:int, colour:tuple) -> None:
+		self.rect = pygame.Rect(x, y, w, h)		# create a rectangle at 0,0 measuring w x h	
+		self.colour = colour					# set colour
 		self.rect.bottom = y
 		self.rect.centerx = x
 		self.speedy = 1000
@@ -14,18 +13,15 @@ class Bullet():
 		self.active = True
 		
 	def get_rect(self):
-		return self.rect	
+		return self.rect
 
-	def update(self, dt) -> bool:
+	def update(self, dt) -> None:
 		self.rect.y -= round(self.speedy * dt)
 		# delete if moves off top of screen
 		if self.rect.bottom < 0:
 			self.active = False
 			
 		return self.active
-	
+			
 	def draw(self) -> None:
-		if shared.debug:
-			pygame.draw.rect(shared.screen, shared.YELLOW, self.rect, 1)
-		else:
-			shared.screen.blit(self.image, self.rect)
+		pygame.draw.rect(shared.screen, self.colour, self.rect)
