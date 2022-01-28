e909cc4747c1a2720f87a8b432007b9df699d519
Inksaver <Inksaver@users.noreply.github.com>
2022-01-28 06:46:01 +0000

    Update Shared.cs

--- Episode02-Add_Mobs/Monogame/Shared.cs
+++ Episode02-Add_Mobs/Monogame/Shared.cs
@@ -1,6 +1,4 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 
 namespace Shmup
@@ -12,13 +10,13 @@ namespace Shmup
         /// </summary>
         public static int WIDTH = 480;      // Window width set here
         public static int HEIGHT = 600;     // Window height set here
-        public static Dictionary<string, int> gameStates = new Dictionary<string, int>
+        public static Dictionary<string, int> GameStates = new Dictionary<string, int>
         {
             {"menu", 1},
             {"play", 2},
             {"quit", 3}
         };
-        public static int gameState = 1;    // default gamestate (menu)
+        public static int GameState = 1;    // default gamestate (menu)
         public static Color GREEN = new Color(0,255,0); // Color.Green is (0, 128, 0) half as bright
     }
 }
