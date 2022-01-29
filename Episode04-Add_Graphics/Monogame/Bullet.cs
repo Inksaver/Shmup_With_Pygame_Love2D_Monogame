31b1f14884ed2aff16d0095f5726b4373f8f6cf4
Inksaver <Inksaver@users.noreply.github.com>
2022-01-29 13:52:09 +0000

    Add files via upload

---
+++ Episode04-Add_Graphics/Monogame/Bullet.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace Shmup
+{
+    internal class Bullet
+    {
+        /// <summary>
+        /// Mob class for generating meteors from top of window
+        /// </summary>
+
+		#region Class variables
+        public bool Active { get; set; }
+        public RectangleF Rectangle;
+        private Texture2D bulletImg;
+        private float speedY;
+        #endregion
+        #region Constructor
+        public Bullet(GraphicsDevice graphicsDevice, Texture2D bulletimg, float speed)
+        {
+            bulletImg = bulletimg;
+            Rectangle = new RectangleF
+            (
+                Player.Rectangle.X + (Player.Rectangle.Width - bulletImg.Width) / 2,
+                Player.Rectangle.Y - bulletImg.Height,
+                bulletImg.Width,
+                bulletImg.Height
+            );
+            speedY = speed;
+            if (Shared.Debug) speedY = speed / 2;
+            Active = true;
+        }
+        #endregion
+        #region Class Methods
+        public bool Update(float dt)
+        {
+            Rectangle.Y -= speedY * dt;
+            if (Rectangle.Y < 0 - Rectangle.Height)
+                Active = false;
+            return Active;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Shared.Debug)
+                spriteBatch.DrawRectangle(Rectangle, Color.Yellow); //debug check image position
+            else
+                spriteBatch.Draw(bulletImg, Rectangle.Position, Color.White);
+        }
+        #endregion
+    }
+}
---
+++ Episode04-Add_Graphics/Monogame/Mob.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace Shmup
+{
+    internal class Mob
+    {
+        /// <summary>
+        /// Mob class for generating meteors from top of window
+        /// </summary>
+        #region Class variables
+        public RectangleF Rectangle;
+        private Texture2D meteorImg;
+        private float speedY;
+        private float speedX;
+        private Random random;
+        #endregion
+        #region Constructor
+        public Mob(GraphicsDevice graphicsDevice, Texture2D img)
+        {
+            random = new Random();
+            meteorImg = img;
+            Rectangle = new RectangleF(0, 0, img.Width, img.Height);
+            SetProperties();
+        }
+        #endregion
+        #region Class Methods
+        public void SetProperties()
+        {
+            Rectangle.X = random.Next(0, Shared.WIDTH - (int)Rectangle.Width);
+            Rectangle.Y = random.Next(-150, -50);
+            speedY = random.Next(50, 300);
+            speedX = random.Next(-50, 50);
+            if (Shared.Debug)
+            {
+                speedY = random.Next(10, 100);
+                speedX = random.Next(-20, 20);
+            }
+        }
+        public void Update(float dt)
+        {
+            Rectangle.X += speedX * dt;
+            Rectangle.Y += speedY * dt;
+            if (Rectangle.Y > Shared.HEIGHT || Rectangle.X < 0 - Rectangle.Width || Rectangle.X > Shared.WIDTH)
+                SetProperties();
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(meteorImg, Rectangle.Position, Color.White);
+            if(Shared.Debug)
+                spriteBatch.DrawRectangle(Rectangle, Color.Blue); //debug check image position
+        }
+        #endregion
+    }
+}
